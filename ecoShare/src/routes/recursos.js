d31ef4eeaaeb8cbdecf6390488b3d0b2cab30d9a
Weslley <takaiwesll@gmail.com>
2026-06-07 01:14:39 -0300

    corrige ordem das rotas para evitar conflito com /:id

--- ecoShare/src/routes/recursos.js
+++ ecoShare/src/routes/recursos.js
@@ -5,24 +5,28 @@ const sc = require("../controllers/solicitacoesController");
 const { autenticar } = require("../middleware/auth");
 
 router.get("/", rc.listar);
-router.get("/:id", rc.detalhe);
 
 router.get("/meus", autenticar, rc.meus);
 router.get("/novo", autenticar, rc.paginaCriar);
 router.post("/novo", autenticar, rc.criar);
-router.get("/:id/editar", autenticar, rc.paginaEditar);
-router.post("/:id/editar", autenticar, rc.editar);
-router.post("/:id/deletar", autenticar, rc.deletar);
 
 router.post("/solicitar", autenticar, sc.criar);
 router.get("/minhas/solicitacoes", autenticar, sc.minhasSolicitacoes);
 router.post("/solicitacao/:id/status", autenticar, sc.atualizarStatus);
 
-
-router.get('/perfil', autenticar, (req, res) => {
-  const Usuario = require('../models/Usuario');
+router.get("/perfil", autenticar, (req, res) => {
+  const Usuario = require("../models/Usuario");
   const u = Usuario.findById(req.usuario.id);
-  res.render('recursos/listar', { recursos: [], filtros: {}, titulo: `Perfil de ${u.nome}` });
+  res.render("recursos/listar", {
+    recursos: [],
+    filtros: {},
+    titulo: `Perfil de ${u.nome}`,
+  });
 });
+
+router.get("/:id", rc.detalhe);
+router.get("/:id/editar", autenticar, rc.paginaEditar);
+router.post("/:id/editar", autenticar, rc.editar);
+router.post("/:id/deletar", autenticar, rc.deletar);
 
 module.exports = router;
